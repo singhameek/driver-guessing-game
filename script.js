954fbbfdc812e7e46ceea5fb7ada1393d5a49fe4
singhameek <ameeksinghv@proton.me>
2026-08-20 22:51:05 +0000

    styles.css, script

---
+++ script.js
@@ -0,0 +1,34 @@
+const f1Drivers = [
+    { number: 1, name: "Lando Norris" },
+    { number: 3, name: "Max Verstappen" },
+    { number: 5, name: "Gabriel Bortoleto" },
+    { number: 6, name: "Isack Hadjar" },
+    { number: 10, name: "Pierre Gasly" },
+    { number: 11, name: "Sergio Pérez" },
+    { number: 12, name: "Andrea Kimi Antonelli" },
+    { number: 14, name: "Fernando Alonso" },
+    { number: 16, name: "Charles Leclerc" },
+    { number: 18, name: "Lance Stroll" },
+    { number: 23, name: "Alex Albon" },
+    { number: 27, name: "Nico Hülkenberg" },
+    { number: 30, name: "Liam Lawson" },
+    { number: 31, name: "Esteban Ocon" },
+    { number: 41, name: "Arvid Lindblad" },
+    { number: 43, name: "Franco Colapinto" },
+    { number: 44, name: "Lewis Hamilton" },
+    { number: 55, name: "Carlos Sainz Jr." },
+    { number: 63, name: "George Russell" },
+    { number: 77, name: "Valtteri Bottas" },
+    { number: 81, name: "Oscar Piastri" },
+    { number: 87, name: "Oliver Bearman" }
+];
+
+
+const correctDriver = Math.floor(Math.random() * f1GridNumbers.length);
+
+document.getElementById("number").innerText = correctDriver.number;
+
+let choices = [correctDriver];
+while (choices.length < 4) {
+    let randomDriver = f1Drivers[Math.floor(Math.random() * f1Drivers.length)];
+}
