6508b9971264a44942a217a9e3b300e1dbf91fbe
singhameek <ameeksinghv@proton.me>
2026-08-20 22:15:17 +0000

    this has moved from being a number guessing game to a f1 driver guessing game

--- test.py
+++ test.py
@@ -1,1 +1,16 @@
+import random
+import sys
+while True:
+    x = random.randint(0,100)
 
+    y = int(input("What's your guess? 0-100 \n"))
+    while True:
+        if y > x:
+            print("Lower")
+            y = int(input("What's your guess? \n"))
+        elif y < x:
+            print("Higher")
+            y = int(input("What's your guess? \n"))
+        else:
+            print("Correct!")
+            sys.exit()
